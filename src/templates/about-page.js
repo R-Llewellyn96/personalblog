341a9cac70796a1c695d801b48cb6b3f829012f1
Ryan Llewellyn <ryanz.llewellyn@gmail.com>
2022-05-21 13:50:48 +0100

    About Page Done.

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,26 +3,41 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import {cardMaterial} from "../styling/myCustomStylingComponents";
+import {Box, Card, CardContent, Container, Typography} from "@mui/material";
+
+const boxStyling = {
+  backgroundColor: '#e8f2ed',
+  paddingTop: '4.5rem',
+  paddingBottom: '2rem',
+  paddingLeft: 0,
+  paddingRight: 0,
+  marginLeft: 0,
+  marginRight: 0,
+  maxWidth: '100%',
+  width: '100vw',
+  minHeight: '100vh'
+}
 
 // eslint-disable-next-line
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+      <Box sx={{...boxStyling}}>
+        <Container maxWidth="lg">
+          <Card  sx={{...cardMaterial}}>
+            <CardContent>
+                      <div className="section">
+                        <Typography gutterBottom variant="h4" sx={{fontWeight: 'bold'}} component="div">
+                          {title}
+                        </Typography>
+                        <PageContent className="content" content={content} />
+                      </div>
+            </CardContent>
+          </Card>
+        </Container>
+      </Box>
   );
 };
 

15e336d3dae35dee421105b624149e8550335b1b
Ryan Llewellyn <ryanz.llewellyn@gmail.com>
2022-05-22 15:43:19 +0100

    Updated fonts in theme.js as this is MUI styling location

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,12 @@ import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
 const theme = createTheme({
+    typography: {
+        fontFamily: [
+            'Arial',
+            '"Helvetica Neue"',
+        ].join(','),
+    },
     palette: {
         primary: {
             main: '#556cd6',

e24e1d5fe20d8dfef1b320f9044e45eb71628f66
Ryan Llewellyn <ryanz.llewellyn@gmail.com>
2022-05-22 15:33:34 +0100

    removed roboto as test

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -14,13 +14,6 @@ export default function TopLayout(props) {
                 <title>{title}</title>
                 <meta name="description" content={description} />
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
-                <link rel="preconnect" href="https://fonts.gstatic.com/"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin/>
-                <link
-                    href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
-                    rel="stylesheet"
-                />
-
             </Helmet>
             <ThemeProvider theme={theme}>
                 {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
